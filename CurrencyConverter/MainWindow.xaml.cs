616b1bfa9f4e200d4bde9346de0f8248f5e320fd
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-08-10 06:50:54 +0300

    converting logic, clear and input validation done

--- CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,20 +25,95 @@ namespace CurrencyConverter
         public MainWindow()
         {
             InitializeComponent();
+            BindCurrency();
+        }
+
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+            dtCurrency.Columns.Add("Text");
+            dtCurrency.Columns.Add("Value");
+
+            dtCurrency.Rows.Add("--SELECT--", 0);
+            dtCurrency.Rows.Add("INR",1);
+            dtCurrency.Rows.Add("USD", 75);
+            dtCurrency.Rows.Add("EUR", 85);
+            dtCurrency.Rows.Add("SAR",20);
+            dtCurrency.Rows.Add("POUND",5);
+            dtCurrency.Rows.Add("DEM",43);
+
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.SelectedValuePath = "Value";
+            cmbFromCurrency.SelectedIndex = 0;
+
+            cmbToCurrency.ItemsSource=dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath="Value";
+            cmbToCurrency.SelectedIndex = 0;
         }
 
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
+            double ConvertedValue = 0;
+            if (txtCurrency.Text==null || txtCurrency.Text.Trim() == "")
+            {
+                MessageBox.Show("Pleaese Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                txtCurrency.Focus();
+                return;
+            }
+            else if(cmbFromCurrency.SelectedValue==null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                cmbFromCurrency.Focus();
+                return;
+            }
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                cmbFromCurrency.Focus();
+                return;
+            }
+
+            if(cmbFromCurrency.SelectedValue==cmbToCurrency.SelectedValue)
+            {
+                ConvertedValue = double.Parse(txtCurrency.Text);
+
+                lblCurrency.Content=cmbToCurrency.Text+" "+ConvertedValue.ToString("N3");
+            }
+            else
+            {
+                ConvertedValue = (double.Parse(txtCurrency.Text)
+                    *double.Parse(cmbFromCurrency.SelectedValue.ToString()))/double.Parse(cmbToCurrency.SelectedValue.ToString());
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+
+
+            }
 
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-
+            txtCurrency.Text=String.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+            {
+                cmbFromCurrency.SelectedIndex = 0;
+            }
+            if (cmbToCurrency.Items.Count > 0) { 
+               cmbToCurrency.SelectedIndex = 0;
+            }
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
+            Regex regex = new Regex("^[0-9]+");
+            e.Handled = !regex.IsMatch(e.Text);
+
 
         }
     }
